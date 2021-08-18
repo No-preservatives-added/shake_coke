045dde98749af38b5eaff0fe2c0c8b59978987d5
nambatsu <53079276+nambatsu@users.noreply.github.com>
2021-08-18 17:19:45 +0900

    buttonのvisible設定修正

--- Assets/Scripts/Scene02/ButtonAvailable.cs
+++ Assets/Scripts/Scene02/ButtonAvailable.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class ButtonAvailable : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject button;
+    private GameObject coke_button, bottle_button, waterwheel_button, dynamo_button;
+
     void Start()
     {
-        button = GameObject.Find("CokeLevelButton");
+        coke_button = GameObject.Find("CokeLevelButton");
+        bottle_button = GameObject.Find("BottleLevelButton");
+        waterwheel_button = GameObject.Find("WaterWheelLevelButton");
+        dynamo_button = GameObject.Find("DynamoLevelButton");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Data.money>=Data.CokeCost){
-            button.SetActive (true);
+            coke_button.SetActive (true);
         }else{
-            button.SetActive (false);
+            coke_button.SetActive (false);
+        }
+        if(Data.money>=Data.BottleCost){
+            bottle_button.SetActive (true);
+        }else{
+            bottle_button.SetActive (false);
+        }
+        if(Data.money>=Data.WaterWheelCost){
+            waterwheel_button.SetActive (true);
+        }else{
+            waterwheel_button.SetActive (false);
+        }
+        if(Data.money>=Data.DynamoCost){
+            dynamo_button.SetActive (true);
+        }else{
+            dynamo_button.SetActive (false);
         }
     }
 }
