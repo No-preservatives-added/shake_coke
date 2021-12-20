48c5e48f8d971f4b1458439a3a15f26fa2f7056e
pimanaya <logicr_music@yahoo.co.jp>
2021-12-20 21:26:41 +0900

    MoneyDisplayを修正（単位配列は途中まで）、金を増やすボタン1回で1234無量大数増えるように変更

--- Assets/Scripts/Scene02/MoneyDisplay.cs
+++ Assets/Scripts/Scene02/MoneyDisplay.cs
@@ -8,9 +8,13 @@ using System.Numerics;
 public class MoneyDisplay : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Text moneytext;
-    private int money_string_text = 0;
-    
+    public Text moneyText;
+    private string moneyString = "";
+    private int moneyStringLength = 0;
+    private int moneyOffset = 0;
+    private int unitNum = 0;
+    private string[] unitList = new string[] { "", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "あ", "い", "う", "え", "お", "か", "き", "無量大数"};
+
 /*
     public static BigInteger kei = 10000000000000000;
     public static BigInteger gai = kei*10000;
@@ -35,11 +39,29 @@ public class MoneyDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moneyString = Data.money.ToString("0"); //文字列変換
+        moneyStringLength = moneyString.Length; //文字数カウント
+        Debug.Log(moneyString+","+moneyStringLength); //確認
 
-        moneytext.text = Data.money.ToString("0");
+        if (moneyStringLength < 69){ //無量大数までの単位で表せる
+            moneyOffset = (moneyStringLength+3)%4+1; //最初の単位までの桁計算1-4
+            unitNum = (int)(moneyStringLength+3)/4-1; //単位の数
+            moneyText.text = moneyString.Substring(0, moneyOffset)+unitList[unitNum]; //表示文字列=最初の単位までの数字+最初の単位
+            for (int i=unitNum;i>0;i--) {
+                moneyText.text += moneyString.Substring(moneyOffset+4*(unitNum-i), 4)+unitList[i-1]; //表示文字列に（単位までの数字+単位）を追加
+                if ((unitNum-i) == 2) break; //単位を4回出力したらやめる
+            }
+        }else{ //無量大数より大きい
+            moneyText.text = moneyString.Substring(0, 1)+"."+moneyString.Substring(1, 8)+"×10^"+(moneyStringLength-1); //1桁.8桁×10^(文字数-1)
+        }
+        moneyText.text += "円"; //表示文字列に円を追加
+
+
+        //moneyText.text = Data.money.ToString("0");
+
         /*
         if (Data.money < 10000){
-        moneytext.text = Data.money.ToString("0");
+        moneyText.text = Data.money.ToString("0");
         }
 
         if (Data.money >= 10000){
