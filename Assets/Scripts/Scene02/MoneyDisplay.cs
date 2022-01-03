ef51e3540860752b0e62dc60cebe86c5015a6d86
alien_ikemen <zhenghaitengtian@gmail.com>
2022-01-03 21:03:33 +0900

    money displayを関数によって表示してます。

--- Assets/Scripts/Scene02/MoneyDisplay.cs
+++ Assets/Scripts/Scene02/MoneyDisplay.cs
@@ -9,12 +9,7 @@ public class MoneyDisplay : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text moneyText;
-    private string moneyString = "";
-    private int moneyStringLength = 0;
-    private int moneyOffset = 0;
-    private int unitNum = 0;
-    private string[] unitList = new string[] { "", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議","無量大数"};
-
+    
     void Start()
     {
 
@@ -23,23 +18,8 @@ public class MoneyDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moneyString = Data.money.ToString("0"); //文字列変換
-        moneyStringLength = moneyString.Length; //文字数カウント
-        //Debug.Log(moneyString+","+moneyStringLength); //確認
-
-        if (moneyStringLength < 73){ //無量大数までの単位で表せる
-            moneyOffset = (moneyStringLength+3)%4+1; //最初の単位までの桁計算1-4
-            unitNum = (int)(moneyStringLength+3)/4-1; //単位の数
-            moneyText.text = moneyString.Substring(0, moneyOffset)+unitList[unitNum]; //表示文字列=最初の単位までの数字+最初の単位
-            for (int i=unitNum;i>0;i--) {
-                moneyText.text += moneyString.Substring(moneyOffset+4*(unitNum-i), 4)+unitList[i-1]; //表示文字列に（単位までの数字+単位）を追加
-                if ((unitNum-i) == 0) break; //単位を2回出力したらやめる
-            }
-        }else{ //無量大数より大きい
-            moneyText.text = moneyString.Substring(0, 1)+"."+moneyString.Substring(1, 8)+"×10^"+(moneyStringLength-1); //1桁.8桁×10^(文字数-1)
-        }
+        moneyText.text = UnitDisplay.Display(Data.money);
         moneyText.text += "円"; //表示文字列に円を追加
-
 
         //moneyText.text = Data.money.ToString("0");
 
--- Assets/Scripts/UnitDisplay.cs
+++ Assets/Scripts/UnitDisplay.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Numerics;
 
-public class UnitDisplay : MonoBehaviour
+public class UnitDisplay
 {
     
-    public static string UnitDisplay(BigInteger x){
-        public string changedText;
-        private string xString = "";
-        private int xStringLength = 0;
-        private int unitOffset = 0;
-        private int unitNum = 0;
-        private string[] unitList = new string[] { "", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議","無量大数"};
+    public static string Display(BigInteger x){
+        string changedText;
+        string xString = "";
+        int xStringLength = 0;
+        int unitOffset = 0;
+        int unitNum = 0;
+        string[] unitList = new string[] { "", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議","無量大数"};
 
         xString = x.ToString("0"); //文字列変換
         xStringLength = xString.Length; //文字数カウント
         //Debug.Log(moneyString+","+moneyStringLength); //確認
 
-        if (unitStringLength < 73){ //無量大数までの単位で表せる
+        if (xStringLength < 73){ //無量大数までの単位で表せる
             unitOffset = (xStringLength+3)%4+1; //最初の単位までの桁計算1-4
             unitNum = (int)(xStringLength+3)/4-1; //単位の数
             changedText = xString.Substring(0, unitOffset)+unitList[unitNum]; //表示文字列=最初の単位までの数字+最初の単位
-            for (int i=unitNum;i>unitNum-1;i--) {//単位を2回出力したらやめる
+            for (int i=unitNum;i>0;i--) {
                 changedText += xString.Substring(unitOffset+4*(unitNum-i), 4)+unitList[i-1]; //表示文字列に（単位までの数字+単位）を追加
+                if ((unitNum-i) == 0) break; //単位を2回出力したらやめる
             }
         }else{ //無量大数より大きい
             changedText = xString.Substring(0, 1)+"."+ xString.Substring(1, 8)+"×10^"+(xStringLength-1); //1桁.8桁×10^(文字数-1)
@@ -31,15 +32,4 @@ public class UnitDisplay : MonoBehaviour
         return changedText;
     }
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
